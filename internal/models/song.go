01c07e3596ce88b9f8a9a9f26f3808ddb898443f
artyom-kalman <p.a.kalashnikov@ya.ru>
2025-02-03 22:24:33 +0900

    Change song model

--- internal/models/song.go
+++ internal/models/song.go
@@ -1,7 +1,9 @@
 package models
 
 type Song struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	GroupId int    `json:"groupId"`
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	GroupId     int    `json:"groupId"`
+	Link        string
+	ReleaseDate string
 }
