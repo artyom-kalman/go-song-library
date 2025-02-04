1879687189fe15ad51130b46f9a30729a9cc5dae
artyom-kalman <p.a.kalashnikov@ya.ru>
2025-02-04 20:34:33 +0900

    Add slog logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,16 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+var Logger *slog.Logger
+
+func InitLogger() {
+	opts := slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}
+
+	Logger = slog.New(slog.NewTextHandler(os.Stdout, &opts))
+}
